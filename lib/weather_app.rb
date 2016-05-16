37745f98acb7444370a1fa0bca69f76b27d5e3ec
nickmandev <nickmandev@gmail.com>
2016-05-16 21:37:39 +0300

    integrating user input

--- lib/weather_app.rb
+++ lib/weather_app.rb
@@ -1,20 +1,28 @@
 require "weather_app/version.rb"
 require 'json'
-require 'city.list.json'
+load 'city.list.json'
 
 module WeatherApp
+attr_accessor :city, :city_id
 
   def self.user_input
     puts "Enter the name of the city starting with capital letter"
-    city = STDIN.gets.chomp
+    @city = STDIN.gets.chomp
   end
 
-  def define_city_id
-    file = JSON
+  def self.define_city_id
+    file = File.read('/Users/nick/Developer/Projects/weather_app/lib/city.list.json')
+    parsed_json = JSON.parse(file, symbolize_names: true)
+    parsed_json.each do |val|
+      if val[:name] == @city
+        @city_id = val[:_id]
+      end
+    end
+
   end
 
   def self.get_data
-    city_id = "727011"
+    @city_id = ""
     api_key = "5250e2389b6cd6d131c5f4f825eafe6d"
     url = "http://api.openweathermap.org/data/2.5/weather?id=#{city_id}&units=metric&APPID=#{api_key}"
     Net::HTTP.get(URI.parse(url))
